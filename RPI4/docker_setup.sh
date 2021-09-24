eb591b841bf2258a5e1cef3614dc93e6cf1c846d
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-24 14:38:55 +0200

    Update docker_setup.sh

--- RPI4/docker_setup.sh
+++ RPI4/docker_setup.sh
@@ -4,14 +4,14 @@
 
 # Dashy
 
-  cd /home/fabsepi/Dockers/Dashy
+  cd /home/fabsepi/Dockers/Dashy || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Docspell
 
-  cd /home/fabsepi/Dockers/Docspell
+  cd /home/fabsepi/Dockers/Docspell || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
@@ -21,7 +21,7 @@
   git clone --branch master https://github.com/ether/etherpad-lite.git
   mv etherpad-lite Dockers/Etherpad
   mv Setup_and_configs/RPI4/Dockers/Etherpad/settings.json Dockers/Etherpad/etherpad-lite
-  cd Dockers/Etherpad/etherpad-lite
+  cd Dockers/Etherpad/etherpad-lite || exit
   chmod u+x src/bin/run.sh
   ./src/bin/run.sh
 
@@ -29,21 +29,21 @@
 
 # Exatorrent
 
-  cd /home/fabsepi/Dockers/Exotorrent
+  cd /home/fabsepi/Dockers/Exotorrent || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Filebrowser
 
-  cd /home/fabsepi/Dockers/Filebrowser
+  cd /home/fabsepi/Dockers/Filebrowser || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Hedgedoc
 
-  cd /home/fabsepi/Dockers/Hedgedoc
+  cd /home/fabsepi/Dockers/Hedgedoc || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
@@ -52,7 +52,7 @@
 
   git clone --branch master https://github.com/leon-ai/leon.git leon
   mv leon Dockers/Leon-AI
-  cd Dockers/Leon-AI/leon
+  cd Dockers/Leon-AI/leon || exit
   pip3 install pipenv
   npm install
   npm run build
@@ -61,49 +61,49 @@
 
 # Nginx-proxy-manager
 
-  cd /home/fabsepi/Dockers/Nginx-proxy-manager
+  cd /home/fabsepi/Dockers/Nginx-proxy-manager || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # OpenHAB
 
-  cd /home/fabsepi/Dockers/OpenHAB
+  cd /home/fabsepi/Dockers/OpenHAB || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Photoprism
 
-  cd /home/fabsepi/Dockers/Photoprism
+  cd /home/fabsepi/Dockers/Photoprism || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Sharry
 
-  cd /home/fabsepi/Dockers/Sharry
+  cd /home/fabsepi/Dockers/Sharry || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Uptime_kuma
 
-  cd /home/fabsepi/Dockers/Uptime_kuma
+  cd /home/fabsepi/Dockers/Uptime_kuma || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Watchtower
 
-  cd /home/fabsepi/Dockers/Watchtower
+  cd /home/fabsepi/Dockers/Watchtower || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Yacht
 
-  cd /home/fabsepi/Dockers/Yacht
+  cd /home/fabsepi/Dockers/Yacht || exit
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
