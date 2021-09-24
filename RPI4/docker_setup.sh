3653d7118cfb2f2fa74d1adb87e6130b0396b99c
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-24 21:59:27 +0200

    Update docker_setup.sh

--- RPI4/docker_setup.sh
+++ RPI4/docker_setup.sh
@@ -42,7 +42,112 @@
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
+# Hedgedoc# Dashy
+
+  cd /home/fabsepi/Dockers/Dashy || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Docspell
+
+  cd /home/fabsepi/Dockers/Docspell || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Etherpad (npm)
+
+  git clone --branch master https://github.com/ether/etherpad-lite.git
+  mv Dockers/Etherpad/settings.json /home/fabsepi
+  mv etherpad-lite Dockers/Etherpad
+  mv /home/fabsepi/settings.json Dockers/Etherpad/etherpad-lite
+  cd Dockers/Etherpad/etherpad-lite || exit
+  chmod u+x src/bin/run.sh
+  ./src/bin/run.sh
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Exatorrent
+
+  cd /home/fabsepi/Dockers/Exotorrent || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Filebrowser
+
+  cd /home/fabsepi/Dockers/Filebrowser || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
 # Hedgedoc
+
+  cd /home/fabsepi/Dockers/Hedgedoc || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Leon-AI (npm)
+
+  git clone --branch master https://github.com/leon-ai/leon.git leon
+  mv leon Dockers/Leon-AI
+  cd Dockers/Leon-AI/leon || exit
+  pip3 install pipenv
+  npm install
+  npm run build
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Nginx-proxy-manager
+
+  cd /home/fabsepi/Dockers/Nginx-proxy-manager || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# OpenHAB
+
+  cd /home/fabsepi/Dockers/OpenHAB || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Photoprism
+
+  cd /home/fabsepi/Dockers/Photoprism || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Sharry
+
+  cd /home/fabsepi/Dockers/Sharry || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Uptime_kuma
+
+  cd /home/fabsepi/Dockers/Uptime_kuma || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Watchtower
+
+  cd /home/fabsepi/Dockers/Watchtower || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Yacht
+
+  cd /home/fabsepi/Dockers/Yacht || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
 
   cd /home/fabsepi/Dockers/Hedgedoc || exit
   docker-compose up -d
