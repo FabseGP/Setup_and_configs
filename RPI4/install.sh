e5745bbc37bf4d06468b0c1866d7d625f6e68952
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-25 15:56:26 +0200

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -56,7 +56,7 @@ EOF
 
   touch ~/.zshrc
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
