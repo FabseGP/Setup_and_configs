d51c6358918c38183796a4b3aa0fdcd17ce5ca48
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-25 15:54:50 +0200

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -72,6 +72,14 @@ EOF
     mv Setup_and_configs/RPI4/podman_setup.sh /home/fabsepi
     chmod u+x podman_setup.sh
   fi
+  
+  for GRP in spi i2c gpio; do
+    addgroup --system $GRP
+  done
+
+  for GRP in adm dialout cdrom audio users video games wheel input tty gpio spi i2c plugdev netdev; do
+    adduser fabsepi $GRP 
+  done
 
   chmod u+x Scripts/*
 
