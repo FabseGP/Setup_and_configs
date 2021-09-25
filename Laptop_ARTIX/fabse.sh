71105432525fef825650f85d126f6f4dc9fcc267
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-25 13:58:11 +0200

    Update fabse.sh

--- Laptop_ARTIX/fabse.sh
+++ Laptop_ARTIX/fabse.sh
@@ -127,7 +127,7 @@
   mkdir -p ../../Extensions
   cp dribbblish-dynamic.js ../../Extensions/.
   spicetify config extensions dribbblish-dynamic.js
-  spicetify config current_theme DribbblishDynamic color_scheme dark
+  spicetify config current_theme DribbblishDynamic color_scheme nord-dark
   spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
   spicetify apply
 
