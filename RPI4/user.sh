a822be648d5b2e3987a1c49587435ab299992e82
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-25 15:54:18 +0200

    Update user.sh

--- RPI4/user.sh
+++ RPI4/user.sh
@@ -2,19 +2,10 @@
 
 # Regular user
 
-  apk add sudo git
-
   for GRP in spi i2c gpio; do
     addgroup --system $GRP
   done
 
-  adduser -s /bin/sh -D fabsepi
-
   for GRP in adm dialout cdrom audio users video games wheel input tty gpio spi i2c plugdev netdev; do
     adduser fabsepi $GRP 
   done
-
-  echo "fabsepi:Alpine54321Linux67890" | /usr/sbin/chpasswd
-
-  echo "%wheel ALL=(ALL) ALL" | (EDITOR="tee -a" visudo)
-  
