bc9b43b3362c9824766c1bea4049d94ac489d9f6
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-25 10:30:16 +0200

    Create docker_setup.sh

---
+++ RPI4/docker_setup.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Dependencies
+
+  pip3 install pipenv
+  pip3 install wheel
+  pip3 install podman-compose
+  sudo ln -sf /home/fabsepi/.local/bin/* /usr/bin
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Dashy
+
+  cd /home/fabsepi/Dockers/Dashy || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Docspell
+
+  cd /home/fabsepi/Dockers/Docspell || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Etherpad (npm)
+
+  git clone --branch master https://github.com/ether/etherpad-lite.git
+  mv Dockers/Etherpad/settings.json /home/fabsepi
+  mv etherpad-lite Dockers/Etherpad
+  mv /home/fabsepi/settings.json Dockers/Etherpad/etherpad-lite
+  cd Dockers/Etherpad/etherpad-lite || exit
+  chmod u+x src/bin/run.sh
+  ./src/bin/run.sh
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Exatorrent
+
+  cd /home/fabsepi/Dockers/Exotorrent || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Filebrowser
+
+  cd /home/fabsepi/Dockers/Filebrowser || exit
+  mkdir config
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Hedgedoc
+
+  cd /home/fabsepi/Dockers/Hedgedoc || exit
+  mkdir data
+  mkdir config
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Leon-AI (npm)
+
+  git clone --branch master https://github.com/leon-ai/leon.git leon
+  mv leon Dockers/Leon-AI
+  cd Dockers/Leon-AI/leon || exit
+  npm install
+  npm run build
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Nginx-proxy-manager
+
+  cd /home/fabsepi/Dockers/Nginx-proxy-manager || exit
+  mkdir data/nginx
+  mkdir letsencrypt
+  mkdir data/mariadb
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# OpenHAB
+
+  cd /home/fabsepi/Dockers/OpenHAB || exit
+  mkdir openhab_addons
+  mkdir openhab_conf
+  mkdir openhab_userdata
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Photoprism
+
+  cd /home/fabsepi/Dockers/Photoprism || exit
+  mkdir data
+  mkdir database
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Sharry
+
+  cd /home/fabsepi/Dockers/Sharry || exit
+  mkdir postgres_data
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Uptime_kuma
+
+  cd /home/fabsepi/Dockers/Uptime_kuma || exit
+  mkdir uptime-kuma
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Watchtower
+
+  cd /home/fabsepi/Dockers/Watchtower || exit
+  podman-compose up -d
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Yacht
+
+  cd /home/fabsepi/Dockers/Yacht || exit
+  mkdir yacht
+  podman-compose up -d
