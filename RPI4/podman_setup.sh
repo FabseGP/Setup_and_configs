0424978cb5b1d143ca590b39e33bc84420e8ebd0
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-26 16:14:24 +0200

    Update podman_setup.sh

--- RPI4/podman_setup.sh
+++ RPI4/podman_setup.sh
@@ -82,9 +82,9 @@
 # Nginx-proxy-manager
 
   cd /home/fabsepi/Dockers/Nginx-proxy-manager || exit
-  mkdir data/nginx
+  mkdir -p data/nginx
   mkdir letsencrypt
-  mkdir data/mariadb
+  mkdir -p data/mariadb
   podman-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
