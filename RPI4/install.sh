5422f772ac5220cd512c5c56669fa2812b6c69c4
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-27 14:53:40 +0200

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -136,7 +136,7 @@ EOF
   chmod 600 /swapfile
   mkswap /swapfile
   swapon /swapfile
-  echo '/swapfile   none    swap    sw    0   0' | sudo tee -a /etc/fstab
+  echo '/swapfile   none    swap    sw    0   0' | sudo tee -a /etc/fstab > /dev/null
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
