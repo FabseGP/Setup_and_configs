2f1070405af9aba76e345113556aff45ef4311f3
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-27 09:45:03 +0200

    Update install.sh

--- RPI4/install.sh
+++ RPI4/install.sh
@@ -160,6 +160,7 @@ EOF
   (crontab -l; echo "@reboot /home/fabsepi/Scripts/syncthing.sh")|awk '!x[$0]++'|crontab -
   (crontab -l; echo "@reboot /home/fabsepi/Scripts/leon.sh")|awk '!x[$0]++'|crontab -
   (crontab -l; echo "@reboot /home/fabsepi/Scripts/etherpad.sh")|awk '!x[$0]++'|crontab -
+  (crontab -l; echo "@reboot /home/fabsepi/Scripts/dashy.sh")|awk '!x[$0]++'|crontab -
   sudo /bin/bash -c 'echo "@reboot /home/fabsepi/Scripts/seagate.sh" >> sudo /etc/crontab'
   (crontab -l; echo "@reboot /home/fabsepi/Scripts/pipewire.sh")|awk '!x[$0]++'|crontab -
 
