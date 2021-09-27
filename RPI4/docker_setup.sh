fdd3ddbd802fbc2ab552d964b87e65bb2e8af9fc
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-09-27 15:50:31 +0200

    Update docker_setup.sh

--- RPI4/docker_setup.sh
+++ RPI4/docker_setup.sh
@@ -48,7 +48,6 @@
 # Grafana
 
   cd /home/fabsepi/Dockers/Grafana || exit
-  docker volume create grafana-storage
   docker-compose up -d
 
 #----------------------------------------------------------------------------------------------------------------------------------
