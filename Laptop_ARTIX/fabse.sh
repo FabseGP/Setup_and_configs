30c8ab26750d61dad2c64c5e4539469c1577eeeb
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-10-02 16:06:32 +0200

    Update fabse.sh

--- Laptop_ARTIX/fabse.sh
+++ Laptop_ARTIX/fabse.sh
@@ -2,9 +2,11 @@
 
 # Package-installation
 
+  pacman -S artix-archlinux-support
+
   cp /home/fabse/Konfiguration/Scripts/Installation/Config/pacman.conf /etc/pacman.conf
 
-  pacman -S terminator nautilus i3status-rust fzf curl wget gammastep tlp-runit foliate neovim zsh zsh-autosuggestions zsh-syntax-highlighting bashtop spectacle zathura zathura-pdf-poppler pipewire pipewire-alsa pipewire-pulse easyeffects pavucontrol sway swaylock arduino arduino-avr-core openshot mousepad wine wine-mono wine-gecko kicad-library kicad-library-3d links gnome-sudoku gnome-mahjongg gnome-calculator cups-runit dolphin dolphin-plugins qutebrowser geogebra kalzium step neofetch gthumb unrar unzip texlive-most atom libreoffice-fresh ark sddm-runit nodejs rclone syncthing-runit discord wayland gimp plasma ffmpegthumbs kdegraphics-thumbnailers linux-firmware alsa-utils networkmanager-runit alacritty stlink rsync asp lutris qemu virt-manager libvirt libvirt-python virt-install xdg-desktop-portal-kde xdg-desktop-portal-wlr pipewire-media-session gnuplot python3 realtime-privileges libva-intel-driver brightnessctl wl-clipboard ld-lsb lsd imv freecad artools iso-profiles aisleriot bsd-games vlc ufw brave obs-studio firefox kicad libpipewire02 polkit-gnome waybar moc artix-archlinux-support fcron-runit steam mypaint slurp grim android-tools qemu-user-static figlet shellcheck kdialog bitwarden jdk-openjdk
+  pacman -Syu terminator nautilus i3status-rust fzf curl wget gammastep foliate neovim zsh zsh-autosuggestions zsh-syntax-highlighting bashtop spectacle zathura zathura-pdf-poppler pipewire pipewire-alsa pipewire-pulse easyeffects pavucontrol sway swaylock arduino arduino-avr-core openshot mousepad wine wine-mono wine-gecko kicad-library kicad-library-3d links gnome-sudoku gnome-mahjongg gnome-calculator cups-runit dolphin dolphin-plugins qutebrowser geogebra kalzium step neofetch gthumb unrar unzip texlive-most atom libreoffice-fresh ark nodejs rclone syncthing-runit wayland gimp plasma ffmpegthumbs kdegraphics-thumbnailers linux-firmware alsa-utils networkmanager-runit alacritty rsync asp lutris qemu virt-manager libvirt libvirt-python virt-install xdg-desktop-portal-kde xdg-desktop-portal-wlr pipewire-media-session gnuplot python3 python-pip realtime-privileges libva-intel-driver brightnessctl wl-clipboard ld-lsb lsd imv freecad artools iso-profiles aisleriot bsd-games vlc ufw brave-bin obs-studio firefox kicad libpipewire02 polkit-gnome waybar moc fcron-runit steam mypaint slurp grim android-tools qemu-user-static figlet shellcheck kdialog bitwarden jdk-openjdk
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -16,8 +18,8 @@
 
 # Files for stm32x
 
-  xdg-open https://www.st.com/en/development-tools/stm32cubeide.html
-  xdg-open https://www.st.com/en/development-tools/stm32cubemx.html
+  sudo --user=fabse xdg-open https://www.st.com/en/development-tools/stm32cubeide.html
+  sudo --user=fabse xdg-open https://www.st.com/en/development-tools/stm32cubemx.html
 
   read -rp "Are you ready again? Type anything for yes: " STM32_ready
 
@@ -25,7 +27,7 @@
 
 # Installation of packages from AUR
 
-  yay -S spicetify-cli-git spotify stm32cubemx stm32flash bastet cbonsai stm32cubemonitor stm32cubeide stm32cubemx openrgb osp-tracker balena-etcher onlyoffice-bin standardnotes-bin toilet
+  yay -S spicetify-cli spotify bastet cbonsai stm32cubeide stm32cubemx openrgb osp-tracker balena-etcher onlyoffice-bin standardnotes-bin revolt-desktop toilet
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -34,54 +36,54 @@
   chsh -s /usr/bin/zsh fabse
   chsh -s /usr/bin/zsh root
 
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+  sudo --user=fabse git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+  sudo --user=fabse echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' | sudo --user=fabse tee -a /home/fabse/.zshrc > /dev/null
 
-  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-  mv 'MesloLGS NF Regular.ttf' MesloLGS.ttf
-  wget https://www.opensans.com/download/open-sans.zip
-  unzip open-sans.zip
+  sudo --user=fabse wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+  sudo --user=fabse mv 'MesloLGS NF Regular.ttf' MesloLGS.ttf
+  sudo --user=fabse wget https://www.opensans.com/download/open-sans.zip
+  sudo --user=fabse unzip open-sans.zip
 
-  mkdir ~/.local/share/fonts
-  mv OpenSans-Regular.ttf ~/.local/share/fonts/OpenSans.ttf
-  mv MesloLGS.ttf ~/.local/share/fonts/MesloLGS.ttf
+  sudo --user=fabse mkdir ~/.local/share/fonts
+  sudo --user=fabse mv OpenSans-Regular.ttf ~/.local/share/fonts/OpenSans.ttf
+  sudo --user=fabse mv MesloLGS.ttf ~/.local/share/fonts/MesloLGS.ttf
 
-  fc-cache -f -v
+  sudo --user=fabse fc-cache -f -v
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Grub-theme
 
-  git clone https://github.com/vinceliuice/grub2-themes.git
-  cd grub2-themes || return
+  sudo --user=fabse git clone https://github.com/vinceliuice/grub2-themes.git
+  sudo --user=fabse cd grub2-themes || return
 
   ./install.sh -b -t tela
-  cd /home/fabse || return
+  sudo --user=fabse cd /home/fabse || return
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Reveal.js + chart.js + slides.js
 
-  npm install browserify
+  sudo --user=fabse npm install browserify
 
-  git clone https://github.com/hakimel/reveal.js.git
-  cd reveal.js && npm install
+  sudo --user=fabse git clone https://github.com/hakimel/reveal.js.git
+  sudo --user=fabse cd reveal.js && npm install
 
-  cd /home/fabse || return
+  sudo --user=fabse cd /home/fabse || return
 
-  npm install --save slides-js
-  npm install chart.js
+  sudo --user=fabse npm install --save slides-js
+  sudo --user=fabse npm install chart.js
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Maple + chemsketch
 
-  xdg-open https://www.acdlabs.com/resources/freeware/chemsketch/download.php
+  sudo --user=fabse xdg-open https://www.acdlabs.com/resources/freeware/chemsketch/download.php
 
   read -rp "Are you ready again? Type anything for yes: " Science_ready
 
-  unzip /home/fabse/Hentet/ACDLabs202021_ChemSketchFree_Install.zip
-  wine /home/fabse/Hentet/ACDLabs202021_ChemSketchFree_Install/setup.exe
+  sudo --user=fabse unzip /home/fabse/Hentet/ACDLabs202021_ChemSketchFree_Install.zip
+  sudo --user=fabse wine /home/fabse/Hentet/ACDLabs202021_ChemSketchFree_Install/setup.exe
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -93,7 +95,7 @@
 
 # Brave-theme
 
-  xdg-open https://www.deviantart.com/sublime9-design/art/Nord-Theme-for-Chrome-V2-837463227
+  sudo --user=fabse xdg-open https://www.deviantart.com/sublime9-design/art/Nord-Theme-for-Chrome-V2-837463227
 
   read -rp "Are you ready again? Type anything for yes: " Brave_ready
 
@@ -101,37 +103,37 @@
 
 # Firefox-theme
 
-  firefox about:support &
+  sudo --user=fabse firefox about:support &
 
   read -rp "Is the path copied? Then type the full path here: " Firefox_ready
 
-  cp -r /home/fabse/Konfiguration/Scripts/Installation/Config "$Firefox_ready"
-  cp -r /home/fabse/Konfiguration/Scripts/Installation/user.js "$Firefox_ready"
+  sudo --user=fabse cp -r /home/fabse/Konfiguration/Scripts/Installation/Config/chrome "$Firefox_ready"
+  sudo --user=fabse cp -r /home/fabse/Konfiguration/Scripts/Installation/user.js "$Firefox_ready"
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Spicetify
 
-  spotify
+  sudo --user=fabse spotify
 
-  read -rp "Are you ready again? Type anything for yes: " Spotify_ready
+  sudo --user=fabse read -rp "Are you ready again? Type anything for yes: " Spotify_ready
 
   chmod a+wr /opt/spotify
   chmod a+wr /opt/spotify/Apps -R
 
-  git clone https://github.com/morpheusthewhite/spicetify-themes.git
-  cd spicetify-themes || return
-  cp -r -- * ~/.config/spicetify/Themes
+  sudo --user=fabse git clone https://github.com/morpheusthewhite/spicetify-themes.git
+  sudo --user=fabse cd spicetify-themes || return
+  sudo --user=fabse cp -r -- * ~/.config/spicetify/Themes
 
-  cd "$(dirname "$(spicetify -c)")/Themes/DribbblishDynamic" || return
-  mkdir -p ../../Extensions
-  cp dribbblish-dynamic.js ../../Extensions/.
-  spicetify config extensions dribbblish-dynamic.js
-  spicetify config current_theme DribbblishDynamic color_scheme nord-dark
-  spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
-  spicetify apply
+  sudo --user=fabse cd "$(dirname "$(spicetify -c)")/Themes/DribbblishDynamic" || return
+  sudo --user=fabse mkdir -p ../../Extensions
+  sudo --user=fabse cp dribbblish-dynamic.js ../../Extensions/.
+  sudo --user=fabse spicetify config extensions dribbblish-dynamic.js
+  sudo --user=fabse spicetify config current_theme DribbblishDynamic color_scheme nord-dark
+  sudo --user=fabse spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
+  sudo --user=fabse spicetify backup apply
 
-  cd /home/fabse || return
+  sudo --user=fabse cd /home/fabse || return
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -149,7 +151,7 @@
 
 # Sway-config
 
-  cd /home/fabse || return
+  sudo --user=fabse cd /home/fabse || return
 
-  cp -r /home/fabse/Konfiguration/Scripts/Installation/Config/i3status-rust .config
-  cp -r /home/fabse/Konfiguration/Scripts/Installation/Config/sway .config
+  sudo --user=fabse cp -r /home/fabse/Konfiguration/Scripts/Installation/Config/i3status-rust .config
+  sudo --user=fabse cp -r /home/fabse/Konfiguration/Scripts/Installation/Config/sway .config
