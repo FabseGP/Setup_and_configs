d63787889b051771d928e7e1fdb5aa89a0967b03
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-10-14 11:29:10 +0000

    Update fabse.sh

--- Laptop_ARTIX/fabse.sh
+++ Laptop_ARTIX/fabse.sh
@@ -18,7 +18,7 @@
 
   doas cp /home/fabse/Setup_and_configs/Laptop_ARTIX/makepkg.conf /etc/makepkg.conf
   mkdir -p /home/fabse/Downloads
-  yay --cleanafter --nouseask -S --stm32cubemx spicetify-cli wayfire-desktop-git bemnu-wlroots avogadroapp bibata-rainbow-cursor-theme handlr-bin candy-icons-git tela-icon-theme sweet-gtk-theme-dark spotify cnrdrvcups-lb kicad-nightly-bin kicad-library-nightly kicad-library-3d-nightly otf-openmoji sunwait-git sway-launcher-desktop swaylock-fancy-git bastet freshfetch-bin cbonsai fuzzel nudoku clipman openrgb-bin osp-tracker macchina onlyoffice-bin standardnotes-bin revolt-desktop toilet arduino-ide-beta-bin
+  yay --cleanafter --nouseask -S stm32cubemx spicetify-cli wayfire-desktop-git bemnu-wlroots avogadroapp bibata-rainbow-cursor-theme handlr-bin candy-icons-git tela-icon-theme sweet-gtk-theme-dark spotify cnrdrvcups-lb kicad-nightly-bin kicad-library-nightly kicad-library-3d-nightly otf-openmoji sunwait-git sway-launcher-desktop swaylock-fancy-git bastet freshfetch-bin cbonsai fuzzel nudoku clipman openrgb-bin osp-tracker macchina onlyoffice-bin standardnotes-bin revolt-desktop toilet arduino-ide-beta-bin
   yay -Scd
   doas archlinux-java set java-17-openjdk
   cd /home/fabse || return 
